3940b46e129a97c94a4e7cc97a515583e08e5077
delhitovizagis1800kms <120730052+delhitovizagis1800kms@users.noreply.github.com>
2025-03-06 16:34:50 +0530

    script

---
+++ customscript2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Customizing MOTD
+sudo sh -c 'echo "  ________ ________   ____       ____     " >> /etc/motd'
+sudo sh -c 'echo " /  _____/ \_____  \ |    |     /  _  \   " >> /etc/motd'
+sudo sh -c 'echo "/   \  ___  /   |   \|    |    /  /_\  \  " >> /etc/motd'
+sudo sh -c 'echo "\    \_\  \/    |    \    |___/    |    \ " >> /etc/motd'
+sudo sh -c 'echo " \______  /\_______  /_______ \____|__  / " >> /etc/motd'
+sudo sh -c 'echo "        \/         \/        \/       \/  " >> /etc/motd'     
